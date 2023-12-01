6523b32c3aa6b4a86c37dcf9c8dfe28286e85224
JoylineDsilva <152602394+JoylineDsilva@users.noreply.github.com>
2023-12-01 16:03:21 +0530

    Add files via upload

---
+++ submitForm.js
@@ -0,0 +1,5 @@
+function submitForm() {
+    // You can add validation and AJAX submission here
+    alert('Form submitted!'); // For demonstration purposes
+    document.getElementById('contactForm').reset(); // Reset the form after submission
+}
